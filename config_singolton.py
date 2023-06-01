3b635d7b1543a0b20bfd9199308642591ba820fc
lara-Crof <Jindata@yamdex.ru>
2023-06-01 10:10:47 +0300

    create singlton_config

---
+++ config_singolton.py
@@ -0,0 +1,46 @@
+'''
+Напишите python class, который читает стандартный файл конфигурации,
+имеющий структуру [секция], ключ=значение.
+2. Напишите конструктор этого класса, который принимает параметр - путь к файлу
+3. Напишите методы класса:
+- get (получение данных конфигурационного файла по секциям, по ключу)
+- reload (перечитывание файла, входящий параметр - путь к файлу)
+Класс должен быть написан в паттерне проектирования Singletone.
+'''
+import configparser
+from typing import Optional
+
+path = 'config.ini'
+
+class ConfigFile(configparser.ConfigParser):
+    """Класс, который читает стандартный файл конфигурации,
+    имеющий структуру [секция], ключ=значение.
+    """
+
+    config = None
+
+    def __new__(cls, *args):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(ConfigFile, cls).__new__(cls)
+        return cls.instance
+    
+    def __init__(self, path: Optional[str]=None) -> None:
+        super().__init__()
+        self.path = path
+        self.config = self.reload()
+
+    
+    def reload(self) -> configparser.SectionProxy:
+        """Перечитывание файла, входящий параметр - путь к файлу."""
+        with open(self.path) as conf:
+            self.config = self.read_file(conf)
+        return self.config
+    
+if __name__ == '__main__':
+    test_conf = ConfigFile(path)
+    print(test_conf.get('APP', 'DEBUG'))
+    test_conf.reload()
+    conf2 = ConfigFile(path)
+    assert test_conf == conf2
+
+    print(test_conf['DATABASE']['HOST'])
